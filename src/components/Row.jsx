69e08d5f3ddef2df0c88ac7ef313479ac4d5817b
Srinivash Dev <srinnivassh@gmail.com>
2023-11-06 23:37:35 +0530

    3

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,11 +1,11 @@
 import axios from 'axios'
 import {FaHeart, FaRegHeart} from 'react-icons/fa'
 import React, { useEffect, useState } from 'react'
+import {MdChevronLeft,MdChevronRight} from 'react-icons/md'
 
 
 
-
-const Row = ({title, fetchURL}) => {
+const Row = ({title, fetchURL,rowId}) => {
 const [movies, setMovies] = useState([])
 const [like, setLike] = useState(false)
 
@@ -17,13 +17,23 @@ useEffect(() =>{
 },[fetchURL])
 
 
+const slideLeft = () => {
+    var slider = document.getElementById('slider' + rowId);
+    slider.scrollLeft = slider.scrollLeft - 500;
+};
+const slideRight = () => {
+    var slider = document.getElementById('slider' + rowId);
+    slider.scrollLeft = slider.scrollLeft + 500;
+};
+
 console.log(movies )
 
   return (
     <div>
         <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-        <div className='relative flex items-center'>
-            <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+        <div className='relative flex items-center group'>
+            <MdChevronLeft onClick={slideLeft} className='bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block left-0 ' size={40}/>
+            <div id={'slider' + rowId} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
                 {movies.map((item, id) => (
                     <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
 
@@ -40,6 +50,8 @@ console.log(movies )
                 ))}
 
             </div>
+
+            <MdChevronRight onClick={slideRight} className='bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block right-0' size={40}/>
         </div>
     </div>
   )
