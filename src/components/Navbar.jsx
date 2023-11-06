fef8202bfb306304bb24ab800e523d83970be1c5
Srinivash Dev <srinnivassh@gmail.com>
2023-11-06 19:53:40 +0530

    hlo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+    <h1 className='text-red-600'>Srinivash</h1>
+      <div>
+        <button>Sign in</button>
+        <button>Sign up</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
