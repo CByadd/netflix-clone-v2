10a8928d7d5db64472b06f8bbf12a924d003c656
Srinivash Dev <srinnivassh@gmail.com>
2023-11-06 23:01:30 +0530

    2

--- src/Requests.js
+++ src/Requests.js
@@ -3,10 +3,10 @@ const key = 'f6929206c59fa46dbe3f913a62e1b988'
 
 const requests = {
     requestPopular:`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
-    requestTopRated:`https://api.themoviedb.org/3/movie/TopRated?api_key=${key}&language=en-US&page=1`,
-    requestTrending:`https://api.themoviedb.org/3/movie/Trending?api_key=${key}&language=en-US&page=1`,
-    requestHorror:`https://api.themoviedb.org/3/movie/Horror?api_key=${key}&language=en-US&page=1`,
-    requestUpcoming:`https://api.themoviedb.org/3/movie/Upcoming?api_key=${key}&language=en-US&page=1`,
+    requestTopRated:`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending:`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestHorror:`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=2`,
+    requestUpcoming:`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
 };
 
 export default requests
